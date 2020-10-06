47fd6fd96d6caf4bb584a5a67c1f9473980860a5
niccholaspage <niccholaspage@gmail.com>
2020-10-06 13:13:10 -0400

    Add comments, optimize calculate function

--- main.s
+++ main.s
@@ -11,34 +11,45 @@
 ;*******************************************************************
 
 	AREA	DATA, READONLY
-MEAN_ARRAY	DCD 1,2,3,4,5,6,7,8,9,10 ; Defines an array named MEAN_ARRAY with 10 elements
+; Defines an array named MEAN_ARRAY with 10 elements in the DATA section
+MEAN_ARRAY	DCD 1,2,3,4,5,6,7,8,9,10
 	AREA    |.text|, CODE, READONLY, ALIGN=2
 	THUMB
-; Not it chief
-; MEAN_ARRAY	DCD 1,2,3,4,5,6,7,8,9,10 ; Defines an array named MEAN_ARRAY with 10 elements
 	EXPORT  Start
 
 Start
 	; Question 1 Testing
 	MOV R0, #0x89AB
 	MOVT R0, #0xCDEF
-	REV R12, R0 ; good result! check against this one.
+	; This converts R0 to 0xAB89 EFCD, converting endian
 	BL ConvertEndian
 	; Question 2 Testing
+	; This calculates the mean of the ten
+	; integers in the array and returns the
+	; mean.
 	BL CalculateMeanOfTenIntegers
 	; Question 3 Testing
+	; This returns -1 if x is negative,
+	; 0 if x == 0,
+	; and +1 if x is positive.
 	MOV R0, #-10
 	BL CalculateFunction
 	; Question 4 Testing
 	MOV R0, #0x7
 	MOV R1, #0x5
 	MOV R2, #0x6
+	; This returns the minimum value
+	; between all parameters.
 	BL FindMinimalValue
 	; Question 5 Testing
 	MOV R0, #5 ; x = 5
 	MOV R1, #10 ; y = 10
+	; The result of this subroutine should
+	; be 5^10 or 9,765,625.
 	BL Power
 
+; Endless loop, since we are an
+; embedded program.
 loop   B    loop
 
 ; Converts between 32-bit little
@@ -46,7 +57,7 @@ loop   B    loop
 ; Parameters:
 ; R0 - Number to convert
 ; Return value:
-; Number converted
+; Number converted (R0)
 ConvertEndian
 	LSR R1, R0, #24 ; Shift R0 to the right by 3 bytes, to isolate the last byte into R1
 	LSL R2, R0, #24 ; Shift R0 to the left by 3 bytes, to isolate the first byte into R2
@@ -58,21 +69,26 @@ ConvertEndian
 	MOV R1, #0xFF00FFFF ; Move our mask into R1 for bit clear
 	BIC R2, R2, R1 ; Perform bit clear on R2 with a mask of R1
 	ORR R0, R3, R2 ; Logical OR R3 and R2, giving us our final reversed result into R0.
-	BX LR
+	BX LR ; Go back to the caller.
 
+; Calculates the mean of the
+; ten integers stored in the
+; MEAN_ARRAY array.
+; Return value:
+; Mean (R0)
 CalculateMeanOfTenIntegers
-	LDR R0, =MEAN_ARRAY
+	LDR R0, =MEAN_ARRAY ; Load the address of the first element in the array into R0
 	MOV R2, #0 ; Our offset - we start at 0
 	MOV R3, #0 ; Our total, adding all the elements
 MeanLoop
-	LDR R1, [R0, R2] ; MEAN_ARRAY[i]
+	LDR R1, [R0, R2] ; R1 = MEAN_ARRAY[i]
 	ADD R3, R3, R1 ; Add our element R1 to our total
 	ADDS R2, #4 ; offset += 4
 	CMP R2, #40 ; We have ten elements in the array, 10 * 4 bytes - 40
 	BLO MeanLoop ; Go back to the loop if we haven't hit 9 yet
 	MOV R2, #10 ; We are gonna divide by 10
 	UDIV R0, R3, R2 ; Divide our total by 10, and store the value in R0, and we are done!
-	BX LR
+	BX LR ; Go back to the caller.
 
 ; Calculate the function as specified
 ; in question 3.
@@ -81,14 +97,13 @@ MeanLoop
 ; Return value:
 ; -1, 0, or 1 (R0)
 CalculateFunction
-	CMP R0, #0
-	BEQ ReturnZero
-	BLT ReturnNegativeOne
-	BGT ReturnPositiveOne
+	CMP R0, #0 ; Check if R0 == 0
+	BEQ ReturnZero ; If it is zero, we branch to return 0.
+	BLT ReturnNegativeOne ; If it is less than 0, return negative 1.
+	MOV R0, #1 ; We want to return 1 because x > 0
+	BX LR ; Go back!
 ReturnNegativeOne
 	MOV R0, #-1 ; Return -1, since x < 0
-ReturnPositiveOne
-	MOV R0, #1
 ReturnZero
 	; Since R0 is already 0, we don't have to set R0 so we are good.
 	BX LR
